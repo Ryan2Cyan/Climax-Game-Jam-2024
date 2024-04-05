87a5e028f70906d7c8e93c09988e1472f6684306
Ryan <cammellryan@gmail.com>
2024-04-05 20:18:17 +0100

    Implemented Unity's New Input system, and set up static InputManager class.

---
+++ Assets/Scripts/Inputs/InputManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace Inputs
+{
+    public class InputManager : MonoBehaviour
+    {
+        public static InputManager Instance;
+
+        public Vector2 Movement;
+        
+        private InputActions _inputs;
+        
+        #region UnityFunctions
+
+        private void Awake()
+        {
+            Instance = this;
+            _inputs = new InputActions();
+        }
+
+        private void OnEnable()
+        {
+            _inputs.Player.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _inputs.Player.Disable();
+        }
+
+        private void FixedUpdate()
+        {
+            Movement = _inputs.Player.Movement.ReadValue<Vector2>();
+            Debug.Log(Movement);
+        }
+
+        #endregion
+
+        // #region EventInvokers
+        //
+        //
+        // #endregion
+    }
+}
