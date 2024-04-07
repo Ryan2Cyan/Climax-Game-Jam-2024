1ca4884541b5f402b9bf04460d9a208593839600
Ryan <cammellryan@gmail.com>
2024-04-07 16:04:56 +0100

    Added Balthazar.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -11,6 +11,8 @@ namespace Player
         public float Smoothing = 1f;
 
         private Vector3 _velocity;
+        private static readonly int Running = Animator.StringToHash("Running");
+
         #region UnityFunctions
 
         private void FixedUpdate()
@@ -21,6 +23,8 @@ namespace Player
             var movementVector = InputManager.Instance.Movement * (Speed * Time.deltaTime);
             var desiredPosition = new Vector3(playerPosition.x + movementVector.x, playerPosition.y, playerPosition.z + movementVector.y);
             transform.position = Vector3.SmoothDamp(playerPosition, desiredPosition, ref _velocity, Smoothing * Time.deltaTime);
+            
+            PlayerManager.Instance.BalthazarAnimator.SetBool(Running, movementVector != Vector2.zero);
         }
 
         #endregion
